f066052365e4122c94c72d8539596fbea4672d7c
tamal <tamal@appscode.com>
2018-04-27 01:36:34 -0700

    added-all

--- main.go
+++ main.go
@@ -8,9 +8,74 @@ import (
 
 func main() {
 	d1 := `{
-   "a": [1, 2]
+  "apiVersion": "apps/v1",
+  "kind": "Deployment",
+  "metadata": {
+    "annotations": {
+      "deployment.kubernetes.io/revision": "1",
+      "docker.io/nginx.git-branch": "master",
+      "docker.io/nginx.maintainer": "NGINX Docker Maintainers <docker-maint@nginx.com>",
+      "docker.io/nginx.repo-url": "github.com/kubepack/pack-server",
+      "docker.io/nginx.version": "1.12"
+    },
+    "labels": {
+      "run": "nginx"
+    },
+    "name": "nginx",
+    "namespace": "default"
+  },
+  "spec": {
+    "replicas": 1,
+    "selector": {
+      "matchLabels": {
+        "run": "nginx"
+      }
+    },
+    "template": {
+      "metadata": {
+        "labels": {
+          "run": "nginx"
+        }
+      },
+      "spec": {
+        "containers": [
+          {
+            "image": "tigerworks/nginx:1.11",
+            "imagePullPolicy": "IfNotPresent",
+            "name": "nginx"
+          }
+        ],
+        "imagePullSecrets": [
+          {
+            "name": "regcred"
+          }
+        ]
+      }
+    }
+  }
 }`
-	p1 := `[{"op":"add","path":"/a/0","value":3}]`
+	p1 := `[
+  {
+    "op": "remove",
+    "path": "/metadata/annotations/docker.io~1nginx.maintainer",
+    "value": null
+  },
+  {
+    "op": "remove",
+    "path": "/metadata/annotations/docker.io~1nginx.repo-url",
+    "value": null
+  },
+  {
+    "op": "add",
+    "path": "/kind",
+    "value": "ReplicaSet"
+  },
+  {
+    "op": "add",
+    "path": "/apiVersion",
+    "value": "extensions/v1beta1"
+  }
+]`
 
 	patch, err := jsonpatch.DecodePatch([]byte(p1))
 	if err != nil {
